6c194b8c4c691dd194fefb0bf6bbe791ece4dc4f
mjsdev5 <mcbridejames196@gmail.com>
2022-03-23 16:45:54 +0200

    fetching Backend Data (GET Requests)

--- util/http.js
+++ util/http.js
@@ -1,8 +1,25 @@
 import axios from "axios";
 
+const BACKEND_URL =
+  "https://rn-expense-d017d-default-rtdb.europe-west1.firebasedatabase.app";
+
 export function storeExpense(expenseData) {
-  axios.post(
-    "https://rn-expense-d017d-default-rtdb.europe-west1.firebasedatabase.app/expenses.json",
-    expenseData
-  );
+  axios.post(BACKEND_URL + "/expenses.json", expenseData);
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + "/expenses.json");
+
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expensesObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expensesObj);
+  }
+  return expenses;
 }
