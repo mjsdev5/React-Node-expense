b0d960607da3d1c023f3732d2bb7cfd4d14b4f35
mjsdev5 <mcbridejames196@gmail.com>
2022-03-23 17:47:11 +0200

    updating and deleting both locally and to the firebase

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -11,10 +11,10 @@ export const ExpensesContext = createContext({
 function expensesReducer(state, action) {
   switch (action.type) {
     case "ADD":
-      const id = new Date().toString() + Math.random().toString();
-      return [{ ...action.payload, id: id }, ...state];
+      return [action.payload, ...state];
     case "SET":
-      return action.payload;
+      const inverted = action.payload.reverse();
+      return inverted;
     case "UPDATE":
       const updatableExpenseIndex = state.findIndex(
         (expense) => expense.id === action.payload.id
@@ -39,7 +39,7 @@ function ExpensesContextProvider({ children }) {
   }
 
   function setExpenses(expenses) {
-    disabled({ type: "SET", payload: expenses });
+    dispatch({ type: "SET", payload: expenses });
   }
 
   function deleteExpense(id) {
